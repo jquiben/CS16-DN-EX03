b06845905b1d8ad2b9e8e98b0fe710c983ef6e72
hadar0x <hadar@illusivenetworks.com>
2016-09-06 01:12:58 +0300

    First Commit

---
+++ C16 Ex03 HadarYudovich 305549503 JonathanQuiben 324424423/Ex03.ConsoleUI/GarageUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.ConsoleUI
+{
+    class GarageUI
+    {
+        public void DisplayMenu()
+        {
+        }
+
+        public void AddVehicleToGarage()
+        {
+        }
+
+        public void DisplayVehicles()
+        {
+        }
+
+        public void ChangeVehicleStatus()
+        {
+        }
+
+        public void InflateVehicleTires()
+        {
+        }
+
+        public void FuelGasolinePoweredVehicle()
+        {
+        }
+
+        public void ChargeElectricPoweredVehicle()
+        {
+        }
+
+        public void DisplayVehicleFullData()
+        {
+        }
+
+    }
+}
---
+++ C16 Ex03 HadarYudovich 305549503 JonathanQuiben 324424423/Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.ConsoleUI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Welcome to the garage!");
+        }
+    }
+}
